e880aa3cbba2e91fd94a8f7fb8f19aec22082788
emanuelsWorld <emanuel.cioara@nexttech.ro>
2021-01-06 12:46:44 +0200

    No more module support :(

--- src/main/java/ro/nexttech/internship/accountancyapp/AccountancyAppApplication.java
+++ src/main/java/ro/nexttech/internship/accountancyapp/AccountancyAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountancyAppApplication {
+public class AccountancyappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountancyAppApplication.class, args);
+        SpringApplication.run(AccountancyappApplication.class, args);
     }
 
 }
