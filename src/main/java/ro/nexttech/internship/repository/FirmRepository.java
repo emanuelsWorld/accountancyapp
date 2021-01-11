61b78f9fe1d7026e5c16266953f8536503dae3da
emanuelsWorld <emanuel.cioara@nexttech.ro>
2021-01-11 15:59:59 +0200

    Added repositories

--- src/main/java/ro/nexttech/internship/repository/FirmRepository.java
+++ src/main/java/ro/nexttech/internship/repository/FirmRepository.java
@@ -1,9 +1,11 @@
 package ro.nexttech.internship.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ro.nexttech.internship.domain.Firm;
+import ro.nexttech.internship.domain.Income;
 
 @Repository
-public interface FirmRepository extends JpaRepository<Firm,Integer> {
+public interface FirmRepository extends JpaRepository<Firm,Integer>, JpaSpecificationExecutor<Firm> {
 }
--- src/main/java/ro/nexttech/internship/repository/UserRepository.java
+++ src/main/java/ro/nexttech/internship/repository/UserRepository.java
@@ -3,8 +3,8 @@ package ro.nexttech.internship.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-import ro.nexttech.internship.domain.Income;
+import ro.nexttech.internship.domain.User;
 
 @Repository
-public interface IncomeRepository extends JpaRepository<Income,Integer>, JpaSpecificationExecutor<Income> {
+public interface UserRepository extends JpaRepository<User,Integer>, JpaSpecificationExecutor<User> {
 }
